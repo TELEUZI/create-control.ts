a874b875353897299e0603fdf4e757ea8c2d07a7
ThunderFlight <igoreshkin2006@gmail.com>
2024-06-24 16:15:40 +0300

    fix: build.mjs

--- template/config/nightwatch-ct/src/components/__tests__/HelloWorld.spec.js
+++ template/config/nightwatch-ct/src/components/__tests__/HelloWorld.spec.js
@@ -4,10 +4,7 @@ describe('Hello World', function () {
   })
 
   it('renders properly', async function () {
-    const welcomeComponent = await browser.mountComponent('/src/components/HelloWorld.vue', {props: {msg: 'Hello Nightwatch'}});
-
-    browser.expect.element(welcomeComponent).to.be.present;
-    browser.expect.element('h1').text.to.contain('Hello Nightwatch');
+    browser.expect.element('h1').text.to.contain('Hello Nightwatch')
   })
 
   after((browser) => browser.end())
